9f965c8a4f6774861b582a0ba4a5c5cbbb713467
jrhovet <jrhovet@gmail.com>
2016-06-13 14:31:08 -0500

    Fixed margins on both sides
    Fixed author line flowing

--- AboutUsViewController.swift
+++ AboutUsViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MessageUI
 
-class AboutUsViewController: UIViewController {
+class AboutUsViewController: UIViewController, MFMailComposeViewControllerDelegate {
 
     //var message:Dictionary<String,String>!
 
@@ -18,9 +19,36 @@ class AboutUsViewController: UIViewController {
     
     var html : String = ""
     
-    @IBAction func ReturnFromAboutUs(sender: AnyObject) {
-        self.performSegueWithIdentifier("FromArticleUnwind", sender: self)
+   
+    
+    @IBAction func SupportEmail(sender: AnyObject) {
+        if !MFMailComposeViewController.canSendMail() {
+            print("Mail services are not available")
+            return
+        }
+        
+        let composeVC = MFMailComposeViewController()
+        composeVC.mailComposeDelegate = self
+        
+        // Configure the fields of the interface.
+        composeVC.setToRecipients(["jhovet18@groton.org"])
+        composeVC.setSubject("Support Question:")
+        
+        
+        // Present the view controller modally.
+        self.presentViewController(composeVC, animated: true, completion: nil)
+        
     }
+    
+    func mailComposeController(controller: MFMailComposeViewController,
+                               didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        // Check the result or perform other tasks.
+        
+        // Dismiss the mail compose view controller.
+        controller.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
